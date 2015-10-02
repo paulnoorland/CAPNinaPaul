58275561ab038a9489e5224ab2023b5ba4534f29
Nina <Ninalijzenga@gmail.com>
2015-10-02 09:44:13 +0200

    changed IIdentifier and Identifier, ignore the interpreter, working on it

--- assignment2/assignment2/Interpreter.java
+++ assignment2/assignment2/Interpreter.java
@@ -4,7 +4,8 @@ import java.io.PrintStream;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class Interpreter {
+
+public class Interpreter<E> {
 	
 	Scanner in = new Scanner(System.in);
 	PrintStream out = new PrintStream(System.out);
@@ -22,7 +23,7 @@ public class Interpreter {
 	    return in.next().charAt(0);
 	}
 	
-	private boolean nextCharIs(Scanner in, char c) {
+	private boolean nextCharIs(Scanner in, char c) {	//Als er iets na de char zit herkent hij hem niet meer..
 	    return in.hasNext(Pattern.quote(c+""));
 	}
 	
@@ -53,13 +54,58 @@ public class Interpreter {
 			throw new APException("Not end of the line");
 		}
 	}
-
 	
-	void start() {
-		String program = in.nextLine();
-		Scanner statementScanner = new Scanner(program);
+	public void parse(List list){
+		
+	}
+	
+	Set factor(Scanner scanner) throws APException {
+		Set result = new Set();
+		if (nextCharIsLetter(in)) {
+			//read identifier
+			//retrieve the set that belongs with that identifier
+		} else if (nextCharIs(in, '{')) {
+			
+			//read a set until '}'
+			//then check whether there is an operator or something
+			//save operator, call factor.
+		} else if (nextCharIs(in, '(')) {
+			//determine the set that is the result of the complex factor
+			//Check all chars until a ')' is present
+			//If a ')' doesn't exist throw error.
+		} else throw new APException("Wrong character");
+		return result;
+	}
+	
+	void saveVariable(Scanner scanner){
+		//the first identifier is the key, the answer to the factor/the set is the value
+		String key = scanner.next();
 		try {
-			Statement statement = statement(statementScanner);
+			Set value = factor();
+		} catch (APException e) {
+			System.out.print(e.getMessage());
 		}
+		//add key-value to the dictionary
+		//dictionary.add(key, value);
+	}
+
+	void start() {
+		while(in.hasNextLine()){
+			String program = in.nextLine();
+			Scanner statementScanner = new Scanner(program);
+			
+			if (nextCharIsLetter(statementScanner)){
+				saveVariable(statementScanner);	//klopt geloof ik
+			} else if (nextCharIs(statementScanner, '/')){
+				//nothing should happen
+			} else if (nextCharIs(statementScanner, '?')){
+				System.out.print(statementScanner.next());
+				//you have to do calculations now, then print something (call factor).
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		new Interpreter().start();
 	}
 }
